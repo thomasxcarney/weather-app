6a54d53f29d9e14ce72fd64eaa06d0387ed275c8
thomasxcarney <thomas.carney26@gmail.com>
2023-10-09 17:12:40 -0400

    Write functions to get forecast info and display
    When you search for a location, the three day forecast info is now
    displayed on the page.

--- scripts.js
+++ scripts.js
@@ -1,14 +1,44 @@
 const locationInput = document.getElementById('location-input');
 const locationSubmit = document.getElementById('submit-location');
+const day1ForecastContainer = document.getElementById('day1');
+const day2ForecastContainer = document.getElementById('day2');
+const day3ForecastContainer = document.getElementById('day3');
 
-const getLocationValue = locationSubmit.addEventListener('click', event => {
+const getLocationValue = function addSubmitEventListener() {
+    locationSubmit.addEventListener('click', event => {
     let location = locationInput.value;
     getWeather(location);
-})
+    })
+};
+
+getLocationValue();
+
+const processForecastDate = function processForecastDataArrWithMap(arr) {
+    const newArr = arr.map(({date, day}) => ({date, day}));
+    return newArr;
+}
+
+const printForecast = function printForecastInfoToContainer(obj, container) {
+    container.innerHTML = '';
+    const date = document.createElement('h4');
+    date.innerHTML = obj.date;
+    const icon = document.createElement('img');
+    icon.src = obj.day.condition.icon;
+    const condition = document.createElement('p');
+    condition.innerHTML = obj.day.condition.text;
+    const avgTempF = document.createElement('p');
+    avgTempF.innerHTML = 'Average Temperature ' + obj.day.avgtemp_f + '&deg;F';
+    container.append(date, icon, condition, avgTempF);
+};
 
 async function getWeather(location) {
     let url = 'https://api.weatherapi.com/v1/forecast.json?key=6828a402ccd64fe9a2f182325230410&days=3&q=' + String(location);
     const response = await fetch(url, {mode: 'cors'})
     const weather = await response.json();
-    console.log(weather.forecast.forecastday)
+    const forecastByDay = weather.forecast.forecastday;
+    let forecastArr = processForecastDate(forecastByDay);
+    console.log(forecastArr);
+    printForecast(forecastArr[0], day1ForecastContainer);
+    printForecast(forecastArr[1], day2ForecastContainer);
+    printForecast(forecastArr[2], day3ForecastContainer);
 };
